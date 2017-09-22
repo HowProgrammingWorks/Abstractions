91093b7514c30e460371a7ba6c3121eef09463dc
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-23 01:46:32 +0300

    Optimize functional examples

--- JavaScript/2-functional-bad.js
+++ JavaScript/2-functional-bad.js
@@ -12,16 +12,22 @@ const fs = require('fs');
 
 const rpad = (s, char, count) => s + char.repeat(count - s.length);
 const lpad = (s, char, count) => char.repeat(count - s.length) + s;
-const padding = [rpad, lpad, lpad, lpad, lpad, lpad, lpad, lpad, lpad];
+const max = (a, b) => (a > b ? a : b);
+const between = (i, a, b) => i >= a && i <= b;
+const padding = [rpad, lpad, lpad, lpad, lpad, lpad];
 const width = [18, 10, 8, 8, 18, 6];
+let maxDensity = 0;
 
 const format = file => (
   fs.readFileSync(file).toString().split('\n')
     .filter((s, i) => i && s)
-    .map(line => line.split(',')
-      .map((cell, i) => padding[i](cell, ' ', width[i]))
-      .join('')
-    ).join('\n')
+    .map(line => line.split(',').map((cell, i, arr) => (
+      !between(i, 1, 3) || (cell = parseInt(cell), arr[i] = cell),
+      i - 3 || (maxDensity = max(maxDensity, cell)), cell
+    ))).map(row => (
+      row.push(Math.round(row[3] * 100 / maxDensity).toString()),
+      row.map((cell, i) => padding[i](cell + '', ' ', width[i])).join('')
+    )).join('\n')
 );
 
 console.log(format('./cities.dat'));
--- JavaScript/2-functional-good.js
+++ JavaScript/2-functional-good.js
@@ -14,12 +14,10 @@ const rpad = (s, char, count) => s + char.repeat(count - s.length);
 const lpad = (s, char, count) => char.repeat(count - s.length) + s;
 const pad = i => (i ? lpad : rpad);
 const width = i => [18, 10, 8, 8, 18, 6][i];
-const readLines = file => fs.readFileSync(file).toString().split('\n');
-const filterRow = (s, i) => (i && s);
+const read = file => fs.readFileSync(file).toString().split('\n');
+const isRow = (s, i) => (i && s);
 const renderCell = (cell, i) => pad(i)(cell, ' ', width(i));
 const renderRow = line => line.split(',').map(renderCell).join('');
-const render = file => (
-  readLines(file).filter(filterRow).map(renderRow).join('\n')
-);
+const render = file => read(file).filter(isRow).map(renderRow).join('\n');
 
 console.log(render('./cities.dat'));

f58d51b437ea6f7f1946e9905f0675ad59e4d0f9
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-26 05:45:41 +0300

    Improve functional example

--- JavaScript/2-functional-good.js
+++ JavaScript/2-functional-good.js
@@ -21,15 +21,14 @@ const renderCell = (cell, i) => cellPad(i, cell + '', cellWidth(i));
 const renderRow = row => row.map(renderCell).join('');
 const renderTable = table => table.map(renderRow).join('\n');
 
-// const densityCol = () => 3;
-const densityCol = 3;
+const densityCol = () => 3;
 const sortByDensity = table => table.sort(
-  (row1, row2) => (row2[densityCol] - row1[densityCol])
+  (row1, row2) => (row2[densityCol()] - row1[densityCol()])
 );
 const calcColumn = (table, max) => table.map(
-  row => (row.push(proportion(max, row[densityCol])), row)
+  row => (row.push(proportion(max, row[densityCol()])), row)
 );
-const calcProportion = table => calcColumn(table, table[0][densityCol]);
+const calcProportion = table => calcColumn(table, table[0][densityCol()]);
 
 const parseTable = lines => lines.map(line => line.split(','));
 const toLines = data => data.split('\n').filter((s, i) => i && s);
